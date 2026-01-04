8b79c5025d14c24e07b4e766170180a9811b2aff
hawkwyk <3226213313@qq.com>
2026-01-04 17:15:45 +0800

    fix device problem in utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@
 Utility functions for the NeRF training pipeline.
 """
 
-
 import torch
 import numpy as np
 
@@ -11,6 +10,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 DEBUG = False
 
 # Utility functions
-img2mse = lambda x, y : torch.mean((x - y) ** 2)
-mse2psnr = lambda x : -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
-to8b = lambda x : (255*np.clip(x,0,1)).astype(np.uint8)
+img2mse = lambda x, y: torch.mean((x - y) ** 2)
+log10 = torch.log(torch.tensor([10.0], device=device))
+mse2psnr = lambda x: -10.0 * torch.log(x) / log10
+to8b = lambda x: (255 * np.clip(x, 0, 1)).astype(np.uint8)
